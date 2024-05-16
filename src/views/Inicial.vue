999c81de22b1cc5dfc22c6b569b1083e77b4e23e
EliasBoaventura <elias4144@hotmail.com>
2024-05-16 15:03:44 -0300

    3

--- src/views/Inicial.vue
+++ src/views/Inicial.vue
@@ -1,15 +1,56 @@
-<template>
-    <div>
-        <h1>tela inicial</h1>
-    </div>
-</template>
+<script setup>
+import { useAuthStore } from "@/stores/auth";
+import { useRouter } from "vue-router";
 
-<script>
-    export default {
-        
-    }
+const authStore = useAuthStore();
+const router = useRouter();
+
+let username = "";
+let password = "";
+
+const login = async () => {
+  try {
+    const { accessToken, expiresIn } = await authStore.login(
+      username,
+      password
+    );
+    console.log("Login successful. Token:", accessToken);
+    console.log("Expira em:", expiresIn);
+    router.push("/home"); // Redirecionar para a página protegida
+  } catch (error) {
+    console.error("Login failed:", error);
+    alert("Login failed. Please try again.");
+    router.push("/login");
+  }
+};
 </script>
 
-<style lang="scss" scoped>
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <label for="username">Username:</label>
+      <input
+        type="text"
+        id="username"
+        v-model="username"
+        required
+      /><br /><br />
+      <label for="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        v-model="password"
+        required
+      /><br /><br />
+      <button type="submit">Login</button>
+    </form>
+    <div v-if="responseData">
+      <p>Resposta do servidor: {{ responseData }}</p>
+    </div>
+  </div>
+</template>
 
+<style scoped>
+/* Estilos específicos para este componente, se necessário */
 </style>
