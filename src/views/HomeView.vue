1899c4c96540b9164c5427428e7a040afbc42e9f
EliasBoaventura <elias4144@hotmail.com>
2024-05-16 20:40:03 -0300

    4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,46 @@
 <template>
-    <div>
-        <H1>Seja bem vindo! Sr. Critovão</H1>
-        <h2>Para ter acesso a essa pagina você precisa estar logado.</h2>
-    </div>
-
+  <div class="welcome-container">
+    <h1 class="welcome-heading">Bem-vindo, Sr. Cristovão!</h1>
+    <h2 class="welcome-subheading">É uma honra tê-lo aqui!</h2>
+    <p class="welcome-message">Você descobriu um portal para aventuras incríveis. Prepare-se para explorar um mundo de possibilidades!</p>
+    <img class="welcome-image" src="./assets/welcome_image.png" alt="Bem-vindo">
+  </div>
 </template>
 
 <script>
-    
-    
+export default {
+  name: 'WelcomePage',
+  // Adicione métodos, propriedades de dados, etc., conforme necessário
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
+.welcome-container {
+  text-align: center;
+  padding: 50px;
+}
 
+.welcome-heading {
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+}
+
+.welcome-subheading {
+  font-size: 1.5rem;
+  color: #666;
+  margin-bottom: 30px;
+}
+
+.welcome-message {
+  font-size: 1.2rem;
+  color: #888;
+  margin-bottom: 30px;
+}
+
+.welcome-image {
+  max-width: 100%;
+  height: auto;
+}
 </style>
